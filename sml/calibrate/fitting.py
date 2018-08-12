7edeed5777ab99a15ae9568684888f4b5c3b59f9
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-08-13 00:05:15 +0800

    Refactor error handling

--- sml/calibrate/fitting.py
+++ sml/calibrate/fitting.py
@@ -5,15 +5,15 @@ from math import sqrt
 
 import numpy as np
 import pandas as pd
-from scipy.optimize import curve_fit, root
+from scipy.optimize import curve_fit, root, OptimizeWarning
 
-from .models import *
+from .defocus import *
 
 logger = logging.getLogger(__name__)
 
 model_dispatch = {
-    'polynomial': PolynomialModel,
-    'huang': HuangModel
+    'polynomial': Polynomial,
+    'huang': Huang
 }
 
 def fit_depth_curve(z, w, model='huang', tol=1e-5):
@@ -35,28 +35,64 @@ def fit_depth_curve(z, w, model='huang', tol=1e-5):
     if model is None:
         raise ValueError("invalid model is provided")
     p0, bounds = model.initial_guess(z, w)
+    logger.debug("p0: {}".format(model))
     try:
         p, _ = curve_fit(model, z, w, p0=p0, bounds=(-np.inf, np.inf), method='trf', xtol=tol)
-    except OptimizeWarning:
-        pass
-    except RuntimeError:
-        logger.error("unable to minimize the result")
+        model.arguments = p
+        logger.debug("popt: {}".format(model))
+    except (OptimizeWarning, RuntimeError):
+        raise RuntimeError("unable to minimize the result")
     except ValueError:
-        logger.error("provided samples contain NaNs")
-
-    model.arguments = p
+        raise ValueError("provided samples contain NaNs")
     return model
 
-def generate_lookup_function(z, w, h, model='huang', tol=1e-5):
+def generate_lookup_function(z, w, h, method='diff', model='huang', tol=1e-5):
     fw = fit_depth_curve(z, w, model, tol)
     fh = fit_depth_curve(z, h, model, tol)
 
     # find intersection
-    def root_func(z):
-        return fw(z)-fh(z)
-    sol = root(root_func, (z.min()+z.max())/2., tol=tol)
+    sol = root(lambda z: fw(z)-fh(z), (z.min()+z.max())/2., tol=tol)
     if not sol.success:
         raise RuntimeError("unable to find z-center")
     elif sol.x.size > 1:
         raise RuntimeWarning("multiple intersections found {}, use first one".format(sol.x))
-    return sol.x[0], fw, fh
+    z0 = sol.x[0]
+
+    # different lookup method
+    def _find_depth_mean(w, h):
+        wsol = root(lambda z: fw(z)-w, z0, tol=tol)
+        hsol = root(lambda z: fh(z)-h, z0, tol=tol)
+        if (not wsol.success) and (not hsol.success):
+            raise RuntimeError("unable to determine z position")
+        return (wsol.x[0]+hsol.x[0])/2.-z0
+
+    def _find_depth_ratio(w, h):
+        sol = root(lambda z: fw(z)/fh(z)-w/h, z0, tol=tol)
+        if not sol.success:
+            print(sol)
+            #raise RuntimeError("unable to determine z position")
+        return sol.x[0]-z0
+
+    def _find_depth_diff(w, h):
+        sol = root(lambda z: (fw(z)-fh(z))-(w-h), z0, tol=tol)
+        if not sol.success:
+            print(sol)
+            #raise RuntimeError("unable to determine z position")
+        return sol.x[0]-z0
+
+    find_depth_method = {
+        'mean': _find_depth_mean,
+        'ratio': _find_depth_ratio,
+        'diff': _find_depth_diff
+    }
+    _find_depth = find_depth_method.get(method)
+    if _find_depth is None:
+        raise ValueError("invalid method is provided")
+
+    # generate the lookup function
+    def find_depth(w, h):
+        if isinstance(w, np.ndarray) and isinstance(h, np.ndarray):
+            return np.array([_find_depth(_w, _h) for _w, _h in np.c_[w, h]])
+        else:
+            return _find_depth(w, h)
+    return find_depth, fw, fh, z0
---
+++ sml/calibrate/lookup.py
@@ -0,0 +1,13 @@
+from abc miport ABCMeta, abstractmethod
+
+class Base(object, metaclass=ABCMeta):
+    pass
+
+class Mean(Base):
+    pass
+
+class Ratio(Base):
+    pass
+
+class Subtract(Base):
+    pass
